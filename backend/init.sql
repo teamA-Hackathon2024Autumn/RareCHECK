adcf2854966fceed7969d6635ec36fd9e06e9a80
Mattani07 <somamatsutani07@gmail.com>
2024-11-16 17:47:11 +0900

    questionsテーブルを作成

--- backend/init.sql
+++ backend/init.sql
@@ -16,6 +16,28 @@ CREATE TABLE categories (
     category_name VARCHAR(255) UNIQUE NOT NULL
 );
 
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    step INTEGER,
+    question VARCHAR(1000) NOT NULL,
+    question_image VARCHAR(255),
+    correct_option VARCHAR(255) NOT NULL,
+    wrong_option_1 VARCHAR(255) NOT NULL,
+    wrong_option_2 VARCHAR(255) NOT NULL,
+    explanation VARCHAR(1000) NOT NULL,
+    explanation_image VARCHAR(255),
+    is_accept BOOLEAN NOT NULL DEFAULT FALSE,
+    difficulty INTEGER, -- 1が易しい、2が普通、3が難しい
+    comment VARCHAR(500),
+    has_comment BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    user_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
 -- 疎通確認用
 -- CREATE TABLE IF NOT EXISTS item (
 --     id INT AUTO_INCREMENT PRIMARY KEY,
