612ecf412fd92c1df4992719f7b17c81c89bf7ee
Mattani07 <somamatsutani07@gmail.com>
2024-11-16 17:44:52 +0900

    疎通確認用のコードをコメントアウト

--- backend/init.sql
+++ backend/init.sql
@@ -1,6 +1,7 @@
-CREATE TABLE IF NOT EXISTS item (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
-);
+-- 疎通確認用
+-- CREATE TABLE IF NOT EXISTS item (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(50) NOT NULL
+-- );
 
-INSERT INTO item (name) VALUES ('Item 1'), ('Item 2'), ('Item 3');
+-- INSERT INTO item (name) VALUES ('Item 1'), ('Item 2'), ('Item 3');
