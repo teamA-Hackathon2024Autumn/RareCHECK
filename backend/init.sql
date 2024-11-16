662eef51c866481e04cc6ab85a2a12cf7320585d
Mattani07 <somamatsutani07@gmail.com>
2024-11-16 17:45:43 +0900

    usersテーブルを作成

--- backend/init.sql
+++ backend/init.sql
@@ -1,3 +1,16 @@
+DROP DATABASE rarecheck;
+
+CREATE DATABASE rarecheck;
+USE rarecheck;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN DEFAULT FALSE
+);
+
 -- 疎通確認用
 -- CREATE TABLE IF NOT EXISTS item (
 --     id INT AUTO_INCREMENT PRIMARY KEY,
