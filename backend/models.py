7e603710584a9df66b502583640ee6d24ae4a6f0
Mattani07 <somamatsutani07@gmail.com>
2024-11-26 23:48:55 +0900

    learning_recordsテーブルに保存するためのモデルを作成

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import and_
 
 db = SQLAlchemy()
 
@@ -15,6 +16,7 @@ class User(db.Model):
     updated_at = db.Column(db.TIMESTAMP, nullable=False, server_default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
     questions = db.relationship('Question', back_populates='user')
+    learning_records = db.relationship('LearningRecord', back_populates='user')
 
 class Category(db.Model):
     __tablename__ = 'categories'
@@ -47,3 +49,16 @@ class Question(db.Model):
 
     user = db.relationship('User', back_populates='questions')
     category = db.relationship('Category', back_populates='questions')
+    learning_records = db.relationship('LearningRecord', back_populates='question')
+
+class LearningRecord(db.Model):
+    __tablename__ = 'learning_records'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date = db.Column(db.TIMESTAMP, nullable=False, server_default=db.func.current_timestamp())
+    is_solved = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'), nullable=False)
+
+    user = db.relationship('User', back_populates='learning_records')
+    question = db.relationship('Question', back_populates='learning_records')
